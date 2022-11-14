441a373db70a85982e0c187289aa20bba04a3da9
RashmivernekarN <rashmivernekar86@gmail.com>
2022-11-14 12:24:30 +0530

    map

---
+++ Assignment2/a13.py
@@ -0,0 +1,4 @@
+#simple if statements
+n = 10
+if n % 2 == 0:
+    print(n,"is an even number")
---
+++ Assignment2/a2.py
@@ -0,0 +1,4 @@
+#write a python program to calculate the length of string
+str=input("enter any string")
+length = len(str)
+print("length of string= ",length)
